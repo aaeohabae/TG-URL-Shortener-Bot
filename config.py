57e1fcb51361360263781ec09609f0ae40786f85
aaeohabae <154266460+aaeohabae@users.noreply.github.com>
2024-05-15 22:07:49 +0545

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", "")) #API ID from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH", "") #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # Bot token from @BotFather
+API_ID = int(os.environ.get("API_ID", "21159773")) #API ID from https://my.telegram.org/auth
+API_HASH = os.environ.get("API_HASH", "49ae08543a07335e195756eba2f56e11") #API Hash from https://my.telegram.org/auth
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6710510477:AAE3YFDMHWT3d6prygYFzlc285OmpQPpZB0") # Bot token from @BotFather
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else [] #Keep thia empty otherwise bot will not work for owner.
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Greylinks")
-DATABASE_URL = os.environ.get("DATABASE_URL", "") # mongodb uri from https://www.mongodb.com/
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://aaroha:aaroha@cluster0.xfupmjy.mongodb.net/?retryWrites=true&w=majority") # mongodb uri from https://www.mongodb.com/
 OWNER_ID =  int(os.environ.get("OWNER_ID", "")) # id of the owner
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(6390495622)
+ADMINS.append(6959948002)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # log channel for information about users
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "2025097901")) # log channel for information about users
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "GreyMattersTech") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "False") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://telegra.ph/file/19eeb26fa2ce58765917a.jpg') # image when someone hit /start
